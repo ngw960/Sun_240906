372e2f198103dc4ed5588ac1acdb69fc90fc0b4b
ngw960 <ngw960@gmail.com>
2024-09-06 09:05:48 +0900

    0906_1

--- asd/urls.py
+++ asd/urls.py
@@ -2,20 +2,35 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 메인메뉴
     path('', views.main_view, name='home'),
+    path('1111/', views.page_1111_view, name='page_1111'),  # 메인메뉴 목록(이름정해야됨)
     path('2222/', views.page_2222_view, name='page_2222'),  # 메인메뉴 목록(이름정해야됨)
     path('3333/', views.page_3333_view, name='page_3333'),  # 메인메뉴 목록(이름정해야됨) 
     path('4444/', views.page_4444_view, name='page_4444'),  # 메인메뉴 목록(이름정해야됨)
     path('5555/', views.page_5555_view, name='page_5555'),  # 메인메뉴 목록(이름정해야됨)
+
+    # 자주 이용하는 서비스
     path('service1/', views.service1_view, name='service1'),  # 자주 이용하는 서비스1(이름 정하기)
     path('service2/', views.service2_view, name='service2'),  # 자주 이용하는 서비스2(이름 정하기)
     path('service3/', views.service3_view, name='service3'),  # 자주 이용하는 서비스3(이름 정하기)
     path('login/', views.login_view, name='login'),  # 로그인
     path('signup/', views.signup_view, name='signup'),  # 회원가입
+
+    # 퀵메뉴(오른쪽 메뉴)
     path('mypage/', views.mypage_view, name='mypage'),  # 마이페이지
-    path('quick_menu2/', views.quick_menu2, name='quick_menu2'),  # 오른쪽 메뉴 2(이름 정하기)
-    path('quick_menu3/', views.quick_menu3, name='quick_menu3'),  # 오른쪽 메뉴 3(이름 정하기)
-    path('quick_menu4/', views.quick_menu4, name='quick_menu4'),  # 오른쪽 메뉴 4(이름 정하기)
-    path('quick_menu5/', views.quick_menu5, name='quick_menu5'),  # 오른쪽 메뉴 5(이름 정하기)
+    path('panel_info/', views.panel_info_view, name='panel_info'),  # 패널 정보
+    path('Last_accessed/', views.Last_accessed_view, name='Last_accessed'),  # 최근 접속
+    path('contact_us/', views.contact_us_view, name='contact_us'),  # 문의 방법
+
+    # 하단 메뉴
+    path('privacy/', views.privacy_view, name='privacy'),  # 개인정보 처리 방침
+    path('terms/', views.terms_view, name='terms'),  # 이용약관
+    path('sitemap/', views.sitemap_view, name='sitemap'),  # 사이트맵
+    path('address/', views.address_view, name='address'),  # 주소
+   
+   # 로그인 페이지
+    path('find_id_pw/', views.find_id_pw_view, name='find_id_pw'),  # 아이디/비밀번호 찾기 페이지
+
 
 ]
--- asd/views.py
+++ asd/views.py
@@ -21,9 +21,14 @@ def login_view(request):
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
+
+# 상단 메뉴
 def main_view(request):
     form = LoginForm()  # 빈 로그인 폼을 전달
     return render(request, 'main.html', {'form': form})
+
+def page_1111_view(request):
+    return render(request, '1111.html')  # 메인메뉴 목록(함수 이름정해야됨)
 
 def page_2222_view(request):
     return render(request, '2222.html')  # 메인메뉴 목록(함수 이름정해야됨)
@@ -37,6 +42,8 @@ def page_4444_view(request):
 def page_5555_view(request):
     return render(request, '5555.html')  # 메인메뉴 목록(함수 이름정해야됨)
 
+
+# 자주 이용하는 서비스
 def service1_view(request):
     return render(request, 'service1.html')  # 자주 이용하는 서비스1(이름 정하기)
 
@@ -53,20 +60,40 @@ def login_view(request):
 def signup_view(request):  # 회원가입 페이지                
     return render(request, 'signup.html')
 
+
+# 퀵메뉴(오른쪽 메뉴)
 def mypage_view(request):  # 마이페이지           
     return render(request, 'mypage.html')
 
-def quick_menu2(request):  # 오른쪽 메뉴2 (이름 정하기)          
-    return render(request, 'quick_menu2.html')
+def panel_info_view(request):  # 패널 정보(퀵 메뉴)
+    return render(request, 'panel_info.html')
 
-def quick_menu3(request):  # 오른쪽 메뉴3 (이름 정하기)          
-    return render(request, 'quick_menu3.html')
+def Last_accessed_view(request):  # 최근 접속(퀵 메뉴)      
+    return render(request, 'Last_accessed.html')
 
-def quick_menu4(request):  # 오른쪽 메뉴4 (이름 정하기)          
-    return render(request, 'quick_menu4.html')
+def contact_us_view(request):  # 문의 방법(퀵 메뉴)   
+    return render(request, 'contact_us.html')
 
-def quick_menu5(request):  # 오른쪽 메뉴5 (이름 정하기)         
-    return render(request, 'quick_menu5.html')
+
+# 하단 메뉴
+def privacy_view(request):  # 개인정보 처리 방침        
+    return render(request, 'privacy.html')
+
+def terms_view(request):  # 이용 약관         
+    return render(request, 'terms.html')
+
+def sitemap_view(request):  # 사이트맵        
+    return render(request, 'sitemap.html')
+
+def address_view(request):  # 주소      
+    return render(request, 'address.html')
+
+
+# 로그인 페이지
+def find_id_pw_view(request):  # 아이디/비밀번호 찾기      
+    return render(request, 'find_id_pw.html')
+
+
 
 
 
